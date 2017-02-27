571a443a20ea4576db51d99c011f481c09174ff5
Ho Cheung <hoc@genebygene.com>
2017-02-27 15:55:23 -0600

    Use atomic_flag

--- main.cpp
+++ main.cpp
@@ -2,18 +2,47 @@
 #include <atomic>
 #include <thread>
 
-std::atomic<bool> done_flag { false };
+bool done_flag = false;
+
+class Lock {
+    std::atomic_flag lock_flag = ATOMIC_FLAG_INIT;
+
+public:
+    void lock() {
+        // Spin until flag is cleared
+        while (lock_flag.test_and_set()) {}
+    }
+
+    void unlock() {
+        lock_flag.clear();
+    }
+};
 
 int main() {
     int num = 1;
 
-    auto thr_proc = [&num]() { num *= 3; done_flag = true; };
+    Lock lock;
+
+    auto thr_proc = [&num, &lock]() {
+        lock.lock();
+        num *= 3;
+        done_flag = true;
+        lock.unlock();
+    };
 
     std::thread thr(thr_proc);
-    while (!done_flag) {};
 
-    thr.join();
+    while (true) {
+        lock.lock();
+
+        if (done_flag) {
+            lock.unlock();
+            break;
+        }
+
+        lock.unlock();
+    }
 
     std::cout << num << std::endl;
-
+    thr.join();
 }
